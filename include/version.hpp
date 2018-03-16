527e4da6ed41a96b1b2703a5908a0598870a613d
Izwan Azmi <izwanazmi90@gmail.com>
2018-03-16 23:07:34 +0800

    Setup cmake

---
+++ include/version.hpp
@@ -0,0 +1,11 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define WaghihRSE_VERSION "0.18.03.16.23"
+#define WaghihRSE_VERSION_SHORT "0.18.03"
+#define WaghihRSE_VERSION_MAJOR 18
+#define WaghihRSE_VERSION_MINOR 03
+#define WaghihRSE_VERSION_RELEASE 16
+#define WaghihRSE_VERSION_BUILD 23
+
+#endif /* VERSION_H */
